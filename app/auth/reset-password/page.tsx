bbb71ed3ee83b5473f210471d27176bbe40ab440
pavan-cyepro <pavansoti.cyepro.trainee@gmail.com>
2026-02-14 12:19:43 +0530

    reset-password

--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next'
 import { ResetPasswordForm } from '@/components/auth/ResetPasswordForm'
+import { Suspense } from 'react'
 
 export const metadata: Metadata = {
   title: 'Reset Password | Groupy',
@@ -8,8 +9,11 @@ export const metadata: Metadata = {
 
 export default function ResetPasswordPage() {
   return (
-    <div className="flex items-center justify-center min-h-screen px-4">
-      <ResetPasswordForm />
-    </div>
+    
+    <Suspense fallback={<div className="text-center py-10">Loading...</div>}>
+      <div className="flex items-center justify-center min-h-screen px-4">
+        <ResetPasswordForm />
+      </div>
+    </Suspense>
   )
 }
